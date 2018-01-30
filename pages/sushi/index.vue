a8094c3350b49db1f6ed79b6a867f46d11a40f95
su-kun1899 <higedrum.coz@gmail.com>
2018-01-30 18:22:09 +0900

    add sushi list page

---
+++ pages/sushi/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="sushi-list">
+  <ul>
+    <li v-for="(item) in sushiList" v-bind:key="item.name">
+      {{item.name}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      sushiList: [{ name: 'まぐろ' }, { name: 'かじき' }]
+    }
+  }
+}
+</script>
