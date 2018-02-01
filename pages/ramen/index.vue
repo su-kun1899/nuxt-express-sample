e4f9f5bba772a7b7850f80c8087fae8a7f77cfe2
su-kun1899 <higedrum.coz@gmail.com>
2018-02-01 18:43:23 +0900

    add page test

--- pages/ramen/index.vue
+++ pages/ramen/index.vue
@@ -1,10 +1,18 @@
 <template>
-  <ramen-list/>
+  <section>
+    <h1>{{message}}</h1>
+    <ramen-list/>
+  </section>
 </template>
 
 <script>
-import ramenList from '~/components/RamenList'
+import ramenList from '@/components/RamenList'
 export default {
+  data: function() {
+    return {
+      message: 'ラーメンリスト'
+    }
+  },
   components: { ramenList }
 }
 </script>
