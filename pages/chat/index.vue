95ff5f445e69369895697f99a5949779be21d2c6
su-kun1899 <higedrum.coz@gmail.com>
2018-02-07 21:19:38 +0900

    WebSocketが動いた気がする

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,62 +1,87 @@
 <template>
   <div>
     <textarea />
-    <p>{{ws.socket.testField}}</p>
+    <!-- <p v-cloak>hogehoge: {{hogehoge}}</p> -->
+    <!-- <p>hoge: {{hoge}}</p> -->
+    <!-- <input type="text" v-bind:value="socket.testField" /> -->
+    <!-- <p v-cloak>socket: {{socket.testField}}</p> -->
+    <p v-cloak>socket: {{testField}}</p>
     <button v-on:click="send">送信</button>
+    <button v-on:click="rerender">反映</button>
   </div>
 </template>
 
 <script>
-var W3CWebSocket = require('websocket').w3cwebsocket
-const socket = new W3CWebSocket('ws://localhost:3000/api/chat', 'echo-protocol')
+// var W3CWebSocket = require('websocket').w3cwebsocket
+// const socket = new W3CWebSocket('ws://localhost:3000/api/chat', 'echo-protocol')
 
-socket.onmessage = function(e) {
-  if (typeof e.data === 'string') {
-    console.log("Received: '" + e.data + "'")
-  }
-}
+// socket.onmessage = function(e) {
+//   if (typeof e.data === 'string') {
+//     console.log("Received: '" + e.data + "'")
+//     this.testField = e.data
+//   }
+// }
 
-class WebsocketClient {
-  constructor() {
-    this.socket = new W3CWebSocket(
-      'ws://localhost:3000/api/chat',
-      'echo-protocol'
-    )
-    this.socket.message = 'hoge'
+// class WebsocketClient {
+//   constructor(socket) {
+//     this.socket = socket
+//     this.socket.testField = 'abc'
 
-    this.testField = 'aaa'
+//     this.socket.onmessage = function(e) {
+//       if (typeof e.data === 'string') {
+//         console.log('message recieved:' + e.data)
+//         this.testField = e.data
+//       }
+//     }
+//   }
 
-    this.socket.onmessage = function(e) {
-      if (typeof e.data === 'string') {
-        console.log('message recieved:' + e.data)
-        this.testField = e.data
-        // this.message = e.data
-      }
-    }
-  }
+//   send() {
+//     this.socket.send('hello')
+//   }
+// }
 
-  send() {
-    this.socket.send('hello')
-  }
+// const ws = new WebsocketClient(socket)
 
-  // setMessage(msg) {
-  //   this.message = msg
-  // }
-}
-
-const ws = new WebsocketClient()
+const W3CWebSocket = require('websocket').w3cwebsocket
 
 export default {
   data: function() {
     return {
-      message: 'Hi',
-      ws: ws
+      // message: 'Hi',
+      // ws: new WebsocketClient(socket)
+      // socket: this.ws.socket
+      socket: new W3CWebSocket('ws://localhost:3000/api/chat', 'echo-protocol'),
+      testField: 'aaa'
+    }
+  },
+  // watch: {
+  //   fuga: function(val) {
+  //     console.log(val)
+  //     this.hogehoge = val.testField
+  //   }
+  //   //   hoge: function() {
+  //   //     console.log('computed: ' + this.ws.socket.testField)
+  //   //     return this.ws.socket.testField
+  //   //   }
+  // },
+  mounted: function() {
+    const self = this
+    this.socket.onmessage = function(e) {
+      if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'")
+        self.testField = e.data
+        // self.$forceUpdate()
+      }
     }
   },
   methods: {
     send: function() {
-      console.log(this.ws)
-      this.ws.send('hello')
+      console.log(this.socket)
+      this.socket.send('hello')
+      // this.$forceUpdate()
+    },
+    rerender: function() {
+      this.$forceUpdate()
     }
   }
 }
