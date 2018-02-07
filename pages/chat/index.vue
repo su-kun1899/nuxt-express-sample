f2be5e4fbe4e1c876488743730775899a76315f5
su-kun1899 <higedrum.coz@gmail.com>
2018-02-07 20:06:31 +0900

    サーバのメッセージを表示する

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <textarea />
-    <p>{{message}}</p>
+    <p>{{ws.socket.testField}}</p>
     <button v-on:click="send">送信</button>
   </div>
 </template>
@@ -16,15 +16,46 @@ socket.onmessage = function(e) {
   }
 }
 
+class WebsocketClient {
+  constructor() {
+    this.socket = new W3CWebSocket(
+      'ws://localhost:3000/api/chat',
+      'echo-protocol'
+    )
+    this.socket.message = 'hoge'
+
+    this.testField = 'aaa'
+
+    this.socket.onmessage = function(e) {
+      if (typeof e.data === 'string') {
+        console.log('message recieved:' + e.data)
+        this.testField = e.data
+        // this.message = e.data
+      }
+    }
+  }
+
+  send() {
+    this.socket.send('hello')
+  }
+
+  // setMessage(msg) {
+  //   this.message = msg
+  // }
+}
+
+const ws = new WebsocketClient()
+
 export default {
   data: function() {
     return {
       message: 'Hi',
-      ws: socket
+      ws: ws
     }
   },
   methods: {
     send: function() {
+      console.log(this.ws)
       this.ws.send('hello')
     }
   }
