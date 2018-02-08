893c18f9491e8c52d54c891c3da19007a48d2e9e
su-kun1899 <higedrum.coz@gmail.com>
2018-02-08 18:52:17 +0900

    メッセージをリストで出すようにした

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <textarea v-model="message"></textarea>
+    <textarea v-model="newMessage"></textarea>
     <br/>
     <button v-on:click="send">送信</button>
-    <p>You recieved message: <b>{{answer}}</b></p>
+    <ul>
+      <li v-for="message in messages" :key="message">
+        {{message}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,21 +19,21 @@ export default {
   data: function() {
     return {
       socket: new W3CWebSocket('ws://localhost:3000/ws/chat'),
-      message: '',
-      answer: ''
+      newMessage: '',
+      messages: []
     }
   },
   created: function() {
     const self = this
     self.socket.onmessage = function(e) {
       if (typeof e.data === 'string') {
-        self.answer = e.data
+        self.messages.push(e.data)
       }
     }
   },
   methods: {
     send: function() {
-      this.socket.send(this.message)
+      this.socket.send(this.newMessage)
     }
   }
 }
